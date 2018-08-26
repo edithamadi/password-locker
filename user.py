1bd2ebd042f7b706b8208acaed6e063892b99364
Edith <you@example.com>
2018-08-26 18:57:37 +0300

    defined properties of objects using init method

--- user.py
+++ user.py
@@ -1,7 +1,17 @@
-class Contact:
+class User:
     """
-    Class that generates new instances of contacts.
+    Class that generates new instances of users.
     """
+user_list = [] # Empty user list
+
+def __init__(self,first_name,last_name,pass_word):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pass_word = pass_word
+
 
 
 
