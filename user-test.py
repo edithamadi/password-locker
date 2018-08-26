389676602339dbdcad4a877d9cc143aba5b96d62
Edith <you@example.com>
2018-08-27 00:22:40 +0300

    added tearDown method to do clean up after each test has run and the test_save_multiple_user to check if we can save multiple user             objects to our user list

--- user-test.py
+++ user-test.py
@@ -2,8 +2,7 @@ import unittest # Importing the unittest module
 from user import User # Importing the user class
 class TestUser(unittest.TestCase):
     
-
-def setUp(self):
+    def setUp(self):
 
         self.new_user = User("Edith","ADR5e") # create user object
 
@@ -18,6 +17,24 @@ def test_save_user(self):
         self.new_user.save_user() # saving the new contact
         self.assertEqual(len(User.user_list),1)
  
+
+def tearDown(self):
+        '''
+        tearDown method which does clean up after each test has run.
+        '''
+        User.user_list = [] 
+
+
+def test_save_multiple_contact(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+
+            self.new_user.save_user()
+            test_user = User("Edith","ADR5e")
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
         
 if __name__ == '__main__':
     unittest.main()
