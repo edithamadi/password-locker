99d0dff43cc1efa93511cb0a1a05b98880bfc8bd
Edith <you@example.com>
2018-08-28 11:16:29 +0300

    debugging errors

--- user-test.py
+++ user-test.py
@@ -1,151 +1,153 @@
-import pyperclip
+# import pyperclip
 import unittest # Importing the unittest module 
 from user import User,Credentials # Importing the user class
 class TestUser(unittest.TestCase):
     
-    def setUp(self):
+        def setUp(self):
 
-        self.new_user = User("Edith","ADR5e") # create user object
+                self.new_user = User("Edith","ADR5e") # create user object
 
-def tearDown(self):
-        '''
-        tearDown method which does clean up after each test has run.
-        '''
-        User.user_list = [] 
+        def tearDown(self):
+                '''
+                tearDown method which does clean up after each test has run.
+                '''
+                User.user_list = [] 
 
-def test_init(self):
+        def test_init(self):
 
-        self.assertEqual(self.new_user.first_name,"Edith")
+                self.assertEqual(self.new_user.first_name,"Edith")
 
-def test_save_user(self):
-       
-        self.new_user.save_user() # saving the new contact
-        self.assertEqual(len(User.user_list),1)
- 
-def test_save_multiple_contact(self):
-            '''
-            test_save_multiple_contact to check if we can save multiple contact
-            objects to our contact_list
-            '''
+        def test_save_user(self):
+        
+                self.new_user.save_user() # saving the new contact
+                self.assertEqual(len(User.user_list),1)
+        
+        def test_save_multiple_contact(self):
+                '''
+                test_save_multiple_contact to check if we can save multiple contact
+                objects to our contact_list
+                '''
 
-            self.new_user.save_user()
-            test_user = User("Test","AD5e")
-            test_user.save_user()
-            self.assertEqual(len(User.user_list),2)
+                self.new_user.save_user()
+                test_user = User("Test","AD5e")
+                test_user.save_user()
+                self.assertEqual(len(User.user_list),2)
 
-def test_delete_user(self):
-        '''
-        test_delete_user to test if it's possible to remove a user from user_list
-        '''
-        self.new_user.save_user()
-        test_user = User("Test","AD5e") #added user
-        test_user.save_user()
+        def test_delete_user(self):
+                '''
+                test_delete_user to test if it's possible to remove a user from user_list
+                '''
+                self.new_user.save_user()
+                test_user = User("Test","AD5e") #added user
+                test_user.save_user()
 
-        self.new_user.delete_user() #deletes user object
-        self.assertEqual(len(User.user_list),1)
+                self.new_user.delete_user() #deletes user object
+                self.assertEqual(len(User.user_list),1)
 
-def test_user_exists(self):
-        '''
-        test to check whether a user exists or not.
-        '''
-        self.new_user.save_user()
-        test_user = User("Test","gL1998") # new user created
-        test_user.save_user()
+        def test_user_exists(self):
+                '''
+                test to check whether a user exists or not.
+                '''
+                self.new_user.save_user()
+                test_user = User("Test","gL1998") # new user created
+                test_user.save_user()
 
-        user_exists =User.user_exists("Test")
+                user_exists =User.user_exists("Test")
 
-        self.assertTrue(user_exists)
+                self.assertTrue(user_exists)
 
-def test_display_users(self):
-        '''
-        method that returns a list of all users saved in the user_list
-        '''
+        def test_display_users(self):
+                '''
+                method that returns a list of all users saved in the user_list
+                '''
 
-        self.assertEqual( User.display_users(),User.user_list)
+                self.assertEqual( User.display_users(),User.user_list)
 
 class TestCredentials(unittest.TestCase):
-      '''
-      Test class that defines test cases for credentials class behaviours.
-      Args:
-      unittest.TestCase:TestCase class helping in creating test cases
-      '''  
 
-def setUp(self):
-        """
-        Set up method to run before each test case.
-        """
-        self.new_credentials = Credentials ("Reddit","ozzyman","JAS5e") #create user object
-
-def tearDown(self):
-        """
-        tearDown method that does the clean-up after each test has run
-        """ 
-        Credentials.credentials_list = []
-
-def test_init(self):
-        """
-        test_init test case to test if the object is initialized properly
-        """
-        self.assertEqual(self.new_credentials.account_name,"edithamadi")
-
-def test_save_credentials(self):
-        """
-        test to check if the credential object is saved on credential list
-        """
-        self.new_credentials.save_credentials() #save user
-        self.assertEqual(len(Credentials.credentials_list),1)
-
-def test_save_multiple_credentials(self):
-        """
-        To check if we can save multiple objects into list
-        """
-        self.new_credentials.save_credentials()
-        test_credentials = Credentials ("Github","Edith","JAS5e")
-        test_credentials.save_credentials()
-        self.assertEqual(len(Credentials.credentials_list),2)
-
-
-def test_delete_credentials(self):
         '''
-        tests if we can delete a credential from our credentials list
+        Test class that defines test cases for credentials class behaviours.
+        Args:
+        unittest.TestCase:TestCase class helping in creating test cases
         '''  
-        self.new_credentials.save_credentials()
-        test_credentials = Credentials("Github","edithamadi","JAS5e")
-        test_credentials.save_credentials()
+                                                                                                                                
 
-        self.new_credentials.delete_credentials() #deletes credentials object
-        self.assertEqual (len(Credentials.credentials_list),1)
+        def setUp(self):
+                """
+                Set up method to run before each test case.
+                """
+                self.new_credentials = Credentials ("edithamadi","amadiey","JAS5e") #create user object
 
-def test_find_credentials_by_account_name(self):
-        '''
-        to check if we can find a credential by the account name and display more information about it
-        '''  
-        self.new_credentials.save_credentials()
-        test_credentials = Credentials("Github","edithamadi","JAS5e")
-        test_credentials.save_credentials()
+        def tearDown(self):
+                """
+                tearDown method that does the clean-up after each test has run
+                """ 
+                Credentials.credentials_list = []
 
-        found_credentials = Credentials.find_by_account_name("Github")
-        self.assertEqual(found_credentials.password,test_credentials.password)
+        def test_init(self):
+                """
+                test_init test case to test if the object is initialized properly
+                """
+                self.assertEqual(self.new_credentials.account_name,"edithamadi")
 
-def test_credentials_exists(self):
-        '''
-        checks if we can return a boolean if we cannot find the credentials
-        '''  
+        def test_save_credentials(self):
+                """
+                test to check if the credential object is saved on credential list
+                """
+                self.new_credentials.save_credentials() #save user
+                self.assertEqual(len(Credentials.credentials_list),1)
 
-        self.new_credentials.save_credentials()
-        test_credentials = Credentials("Github","edithamadi","JAS5e")
-        test_credentials.save_credentials()
+        def test_save_multiple_credentials(self):
+                """
+                To check if we can save multiple objects into list
+                """
+                self.new_credentials.save_credentials()
+                test_credentials = Credentials ("Github","Edith","JAS5e")
+                test_credentials.save_credentials()
+                self.assertEqual(len(Credentials.credentials_list),2)
 
-        credentials_exists =Credentials.credentials_exist("Github")
-        self.assertTrue(credentials_exists)
 
-def test_display_all_credentials(self):
-        '''
-        returns a list of all credentials saved 
-        '''  
+        def test_delete_credentials(self):
+                '''
+                tests if we can delete a credential from our credentials list
+                '''  
+                self.new_credentials.save_credentials()
+                test_credentials = Credentials("Github","edithamadi","JAS5e")
+                test_credentials.save_credentials()
 
-        self.assertEqual( Credentials.display_credentials(),Credentials.credentials_list)
+                self.new_credentials.delete_credentials() #deletes credentials object
+                self.assertEqual (len(Credentials.credentials_list),1)
 
-        if __name__ == '__main__':
-            unittest.main()
+        def test_find_credentials_by_account_name(self):
+                '''
+                to check if we can find a credential by the account name and display more information about it
+                '''  
+                self.new_credentials.save_credentials()
+                test_credentials = Credentials("Github","edithamadi","JAS5e")
+                test_credentials.save_credentials()
+
+                found_credentials = Credentials.find_by_account_name("Github")
+                self.assertEqual(found_credentials.password,test_credentials.password)
+
+        def test_credentials_exists(self):
+                '''
+                checks if we can return a boolean if we cannot find the credentials
+                '''  
+
+                self.new_credentials.save_credentials()
+                test_credentials = Credentials("Github","edithamadi","JAS5e")
+                test_credentials.save_credentials()
+
+                credentials_exists =Credentials.credentials_exist("Github")
+                self.assertTrue(credentials_exists)
+
+        def test_display_all_credentials(self):
+                '''
+                returns a list of all credentials saved 
+                '''  
+
+                self.assertEqual(Credentials.display_all_credentials(),Credentials.credentials_list)
+
+if __name__ == '__main__':
+        unittest.main()
 
--- user.py
+++ user.py
@@ -1,49 +1,49 @@
 class User:
     user_list = [] # Empty user list
 
-def __init__(self,first_name,pass_word):
+    def __init__(self,first_name,pass_word):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.pass_word = pass_word
 
-def save_user(self):
+    def save_user(self):
 
+            '''
+            save_user method saves user objects into contact_list
+            '''
+
+            User.user_list.append(self)
+
+    def delete_user(self):
         '''
-        save_user method saves user objects into contact_list
+        delete_user method deletes a saved user from user_list
+        '''
+        User.user_list.remove(self)
+
+    @classmethod
+    def user_exists(cls,first_name):
+            '''
+            Method that checks if a user exists in the user_list.
+            Args:
+                username: Username to search if it exists
+            Returns :
+                Boolean: True or false depending on if the user exists or not
+            '''
+            for user in cls.user_list:
+                if user.first_name == first_name:
+                        return True
+
+            return False  
+
+    @classmethod
+    def display_users(cls):
+        '''
+        method which returns user list
         '''
 
-        User.user_list.append(self)
-
-def delete_user(self):
-    '''
-    delete_user method deletes a saved user from user_list
-    '''
-    User.user_list.remove(self)
-
-@classmethod
-def user_exists(cls,first_name):
-        '''
-        Method that checks if a user exists in the user_list.
-        Args:
-            username: Username to search if it exists
-        Returns :
-            Boolean: True or false depending on if the user exists or not
-        '''
-        for user in cls.user_list:
-            if user.first_name == first_name:
-                    return True
-
-        return False  
-
-@classmethod
-def display_users(cls):
-    '''
-    method which returns user list
-    '''
-
-    return cls.user_list
+        return cls.user_list
 
 class Credentials:
     """
@@ -67,47 +67,55 @@ class Credentials:
 
     def save_credentials(self):
 
-    '''
-    save_credentials method that saves credentials object into credentials_list
-       '''
-
-    Credentials.credentials_list.append(self)
+        '''
+        save_credentials method that saves credentials object into credentials_list
+        '''
+        Credentials.credentials_list.append(self)
 
     
+
     def delete_credentials(self):
-    '''
-    deletes a saved credential from credentials_list
-    '''
+        '''
+        deletes a saved credential from credentials_list
+        '''
 
-    Credentials.credentials_list.remove(self)
+        Credentials.credentials_list.remove(self)
+        
+    @classmethod
+    def display_all_credentials(cls):
+        '''
+        method that returns a list of all credentials saved
+        '''
 
-@classmethod
-  def find_by_account_name(cls,account_name):
-    '''
-    method that takes in the account name and returns a credential that matches that account name
-    Args:
+        return cls.credentials_list
+
+    @classmethod
+    def find_by_account_name(cls,account_name):
+        '''
+        method that takes in the account name and returns a credential that matches that account name
+        Args:
         account_name:Account name to search for 
-    Returns:
+        Returns:
         Credentials of person that matches the account name
-    '''  
+        '''  
 
-    for credentials in cls.credentials_list:
-      if credentials.account_name == account_name:
-        return credentials
+        for credentials in cls.credentials_list:
+            if credentials.account_name == account_name:
+                return credentials
 
 
-@classmethod
-  def credentials_exist(cls,account_name):
-    '''
-    checks if a credential exists in the credentials list.
-    Args:
-        account_name:Account name to search if it exists
-    Returns:
-        Boolean:True or false depending on if the credential exists    
-    '''      
-    for credentials in cls.credentials_list:
-      if credentials.account_name == account_name:
-        return True
+    @classmethod
+    def credentials_exist(cls,account_name):
+        '''
+        checks if a credential exists in the credentials list.
+        Args:
+            account_name:Account name to search if it exists
+        Returns:
+            Boolean:True or false depending on if the credential exists    
+        '''      
+        for credentials in cls.credentials_list:
+            if credentials.account_name == account_name:
+                return True
 
-    return False  
+                return False  
 
