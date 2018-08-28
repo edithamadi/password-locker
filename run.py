4e8a6432dc4b2102dc7342d0436957b4c50a4bb1
Edith <you@example.com>
2018-08-28 12:19:33 +0300

    created del_user function

--- run.py
+++ run.py
@@ -9,9 +9,18 @@ def create_user(first_name,pass_word): #created the function create_user
     new_user = User(first_name,pass_word) #created new user object and return it
     return new_user
 
-def save_users(user):
+def save_users(user): # created function
     '''
     function to save users
     '''
-    user.save_user() #method to save the user
+    user.save_user() #function calls method to save the user
+
+def del_user(user)
+    '''
+    function to delete a user
+    '''
+    user.delete_user()
+
+
+
 
