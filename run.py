814acf5ec1fdde5e02bde9b77d19b6e0407ba299
Edith <you@example.com>
2018-08-28 12:01:54 +0300

    created save_user function

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ def create_user(first_name,pass_word): #created the function create_user
     new_user = User(first_name,pass_word) #created new user object and return it
     return new_user
 
+def save_users(user):
+    '''
+    function to save users
+    '''
+    user.save_user() #method to save the user
+
